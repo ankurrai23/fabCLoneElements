86aaa4bf4c26aef548f5978488d1e79da56dc8d1
Nikhil Kumar Choudhary <nikhil.kumar@mobcoder.com>
2022-02-25 01:16:24 +0530

    changed the approach for handling the component viewing inside the app

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,29 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import ComponentsDrawer from './ComponentsDrawer';
+import {View, StyleSheet} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+import ComponentList from './ComponentList';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <ComponentsDrawer />
+      <Drawer.Navigator>
+        {ComponentList.map((item, index) => (
+          <Drawer.Screen name={item.name} key={`abc${index}`}>
+            {() => <View style={styles.container}>{item.component}</View>}
+          </Drawer.Screen>
+        ))}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- example/src/ComponentList.js
+++ example/src/ComponentList.js
@@ -1,1 +1,7 @@
-export const COMPONENTS = ['FlightCard', 'FlightOptions'];
+import React from 'react';
+import {FlightCard, FlightOptions} from 'react-native-fab-elements';
+
+export default [
+  {name: 'FlightCard', component: <FlightCard />},
+  {name: 'FlightOptions', component: <FlightOptions />},
+];
