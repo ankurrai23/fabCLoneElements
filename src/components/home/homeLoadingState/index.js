4d4661021c8d3c15b89bf0d4eb3e565589f11f11
@mobcoder-arvind-rashtogi <arvind.rashtogi@mobcoder.com>
2022-04-19 18:18:44 +0530

    loading state

--- src/components/home/homeLoadingState/index.js
+++ src/components/home/homeLoadingState/index.js
@@ -65,7 +65,7 @@ const HomeLoadingState = () => {
         />
       </View>
       <FText type="medium" style={Styles.sectionTitleLoader}>
-        Message
+        Updates
       </FText>
       <InboxPlaceholder />
     </>
