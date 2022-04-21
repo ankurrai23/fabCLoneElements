30bf385aa294e92a1dc83380b6d9560e2cd7181e
Mayank Yadav <mayankyadav@Mayanks-MacBook-Pro.local>
2022-04-22 00:47:03 +0530

    see more added in home

--- src/components/home/headerView/index.js
+++ src/components/home/headerView/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import {FText, FTouchableOpacity} from '../../..';
-import {Color} from '../../../utils/color/index.fabhotel';
-import {DP} from '../../../utils/Dimen';
 import Styles from './Styles';
 
 export default function HeaderView(props) {
@@ -14,7 +11,9 @@ export default function HeaderView(props) {
         {props.title}
       </FText>
       <FTouchableOpacity onPress={props.onPress}>
-        <AntDesign name="rightcircle" size={DP._17} color={Color.DODGER_BLUE} />
+        <FText type="medium" style={Styles.seeMore}>
+          See more
+        </FText>
       </FTouchableOpacity>
     </View>
   );
