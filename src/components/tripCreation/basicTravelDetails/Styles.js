d4dac130cc83df0e4e3fdca347aa63233fc607de
@mobcoder-arvind-rashtogi <arvind.rashtogi@mobcoder.com>
2022-04-29 17:14:02 +0530

    basic travel detail

--- src/components/tripCreation/basicTravelDetails/Styles.js
+++ src/components/tripCreation/basicTravelDetails/Styles.js
@@ -27,4 +27,88 @@ export default StyleSheet.create({
     color: Color.PASTEL_RED,
     marginTop: DP._8,
   },
+  btnStyle: {
+    marginTop: DP._10,
+    borderWidth: 1,
+    borderColor: Color.DODGER_BLUE,
+    backgroundColor: Color.WHITE,
+  },
+  listContainer: {
+    borderWidth: 1,
+    borderColor: Color.LIGHT_PERIWINKLE,
+    borderRadius: DP._8,
+    paddingVertical: DP._16,
+    paddingHorizontal: DP._16,
+    marginTop: DP._6,
+  },
+  sourceTxt: {
+    fontSize: DP._17,
+    lineHeight: DP._21,
+    color: Color.DARK_BLUE,
+  },
+  iteneryView: {
+    height: DP._16,
+    width: DP._63,
+    paddingHorizontal: DP._8,
+    backgroundColor: Color.PURPLEY,
+    borderRadius: DP._9,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itineraryTxt: {
+    fontSize: DP._10,
+    lineHeight: DP._12,
+    color: Color.WHITE,
+  },
+  flex6: {
+    flex: 6,
+  },
+  flexView: (margin) => ({
+    flexDirection: 'row',
+    alignItems: 'center',
+    ...(margin && {
+      marginTop: DP._9,
+    }),
+  }),
+  editTxt: {
+    fontSize: DP._12,
+    lineHeight: DP._14,
+    color: Color.DODGER_BLUE,
+    marginLeft: DP._6,
+  },
+  dateTxt: {
+    fontSize: DP._14,
+    lineHeight: DP._18,
+    color: Color.DARK,
+  },
+  touchView: {
+    top: 10,
+    bottom: 10,
+    left: 20,
+    right: 15,
+  },
+  addCityBtnView: {
+    paddingVertical: DP._8,
+    borderWidth: 1,
+    borderColor: Color.DODGER_BLUE,
+    borderStyle: 'dashed',
+    marginTop: DP._16,
+    borderRadius: DP._8,
+    alignItems: 'center',
+  },
+  addCityTxt: {
+    fontSize: DP._14,
+    lineHeight: DP._18,
+    color: Color.DODGER_BLUE,
+  },
+  removeItinarary: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: DP._15,
+  },
+  removeTxt: {
+    fontSize: DP._14,
+    lineHeight: DP._18,
+    marginLeft: DP._4,
+  },
 });
--- src/components/tripCreation/basicTravelDetails/index.js
+++ src/components/tripCreation/basicTravelDetails/index.js
@@ -1,9 +1,73 @@
 import React from 'react';
 import {View} from 'react-native';
 import {FText, PickerField} from '../../..';
+import Button from '../../../common/components/button';
 import {DP} from '../../../utils/Dimen';
 import {formattedDate} from '../../../utils/Utils';
 import Styles from './Styles';
+import Feather from 'react-native-vector-icons/Feather';
+import {Color} from '../../../utils/color/index.travelPlus';
+import FTouchableOpacity from '../../../common/rn/FTouchableOpacity';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const BasicTravel = ({
+  errors,
+  data,
+  roundTrip,
+  openSourceGoogleSearch,
+  openDestinationGoogleSearch,
+  openDepatureDatePicker,
+  openReturnDatePicker,
+  multiCity,
+}) => {
+  return (
+    <>
+      <PickerField
+        error={errors.sourceError}
+        helperText={errors.sourceTextError}
+        label="From"
+        value={data.source?.name}
+        labelStyle={Styles.textFieldLabel}
+        onPress={openSourceGoogleSearch}
+      />
+      <PickerField
+        error={errors.destinationError}
+        helperText={errors.DestinationTextError}
+        label="Destination"
+        value={data.destination?.name}
+        labelStyle={Styles.textFieldLabel}
+        onPress={openDestinationGoogleSearch}
+      />
+      <View style={Styles.datesContainer}>
+        <PickerField
+          error={errors.defatureDateError}
+          label="Departure date"
+          value={data.departureDate ? formattedDate(data.departureDate) : ''}
+          labelStyle={Styles.textFieldLabel}
+          onPress={openDepatureDatePicker}
+          touchContainer={
+            roundTrip ? {flex: 0.5, marginRight: DP._8} : {flex: 1}
+          }
+        />
+        {roundTrip && (
+          <PickerField
+            label="Return date"
+            value={data.returnDate ? formattedDate(data.returnDate) : ''}
+            error={errors.returnDateError}
+            labelStyle={Styles.textFieldLabel}
+            onPress={openReturnDatePicker}
+            touchContainer={{flex: 0.5, marginLeft: DP._8}}
+          />
+        )}
+      </View>
+      {multiCity && (
+        <Button type="SECONDARY" style={Styles.btnStyle}>
+          Save
+        </Button>
+      )}
+    </>
+  );
+};
 
 function BasicTravelDetails({
   errors,
@@ -16,6 +80,7 @@ function BasicTravelDetails({
   openDepatureDatePicker,
 }) {
   let roundTrip = tripType === 1;
+  let multiCity = tripType === 2;
   return (
     <View style={Styles.container}>
       <FText type="medium" style={Styles.title}>
@@ -36,7 +101,7 @@ function BasicTravelDetails({
         </FText>
       ) : null}
       <View style={Styles.innerContainer}>
-        <PickerField
+        {/* <PickerField
           error={errors.sourceError}
           helperText={errors.sourceTextError}
           label="From"
@@ -73,7 +138,59 @@ function BasicTravelDetails({
               touchContainer={{flex: 0.5, marginLeft: DP._8}}
             />
           )}
+        </View> */}
+
+        {/* <BasicTravel
+          errors={errors}
+          data={data}
+          roundTrip={roundTrip}
+          openSourceGoogleSearch={openSourceGoogleSearch}
+          openDestinationGoogleSearch={openDestinationGoogleSearch}
+          openDepatureDatePicker={openDepatureDatePicker}
+          openReturnDatePicker={openReturnDatePicker}
+          multiCity={multiCity}
+        /> */}
+
+        <View style={Styles.removeItinarary}>
+          <FText type="medium" style={[Styles.removeTxt, {color: Color.DARK}]}>
+            Itinerary 1
+          </FText>
+          <FTouchableOpacity style={Styles.datesContainer}>
+            <AntDesign name="delete" size={DP._16} color={Color.PASTEL_RED} />
+            <FText
+              type="medium"
+              style={[Styles.removeTxt, {color: Color.PASTEL_RED}]}>
+              Remove
+            </FText>
+          </FTouchableOpacity>
         </View>
+
+        <View style={Styles.listContainer}>
+          <View style={Styles.flexView()}>
+            <View style={Styles.flex6}>
+              <FText numberOfLines={1} type="medium" style={Styles.sourceTxt}>
+                Delhi to Mumbai
+              </FText>
+            </View>
+            <View style={Styles.iteneryView}>
+              <FText style={Styles.itineraryTxt}>Itinerary 1</FText>
+            </View>
+          </View>
+          <View style={Styles.flexView('margin')}>
+            <View style={Styles.flex6}>
+              <FText style={Styles.dateTxt}>Thu, 24 Nov</FText>
+            </View>
+            <FTouchableOpacity
+              hitSlop={Styles.touchView}
+              style={Styles.flexView()}>
+              <Feather name="edit-2" size={DP._15} color={Color.DODGER_BLUE} />
+              <FText style={Styles.editTxt}>Edit</FText>
+            </FTouchableOpacity>
+          </View>
+        </View>
+        <FTouchableOpacity style={Styles.addCityBtnView}>
+          <FText style={Styles.addCityTxt}>+ Add a city</FText>
+        </FTouchableOpacity>
       </View>
     </View>
   );
