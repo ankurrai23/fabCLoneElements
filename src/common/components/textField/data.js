3ba69167707276f8f6bfe55ff443997451e8f61d
Chandan Agarwal <chandan.aggarwal@fabhotels.com>
2022-07-20 14:00:24 +0530

    config_changes

--- src/common/components/textField/data.js
+++ src/common/components/textField/data.js
@@ -2,14 +2,14 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import React from 'react';
 import {DP} from '../../../utils/Dimen';
 export default {
-  label: 'Name',
+  label: 'Namddsdsdsdsffcwecqwgevfvwerfvwefvcwevcwevdcxedcwqedwedqwcsace',
   onChangeText: (text) => console.log('Text =>', text),
   helperText: 'error reported',
   placeholder: 'Please enter full name',
   // error: true,
-  icon: <Entypo name="cross" size={DP._24} />,
+  icon: <Entypo name="cross" size={DP._16} />,
   topMargin: 8,
   bottomMargin: 0,
-  rightIcon: <Entypo name="cross" size={DP._24} />,
+  rightIcon: <Entypo name="cross" size={DP._16} />,
   error: true,
 };
