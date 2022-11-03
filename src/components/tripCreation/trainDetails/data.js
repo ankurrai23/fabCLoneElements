a55d468f03eb567bdab9408804573b7cf4299f29
Nikhil Kumar Choudhary <nikhil.kumar@fabhotels.com>
2022-11-03 18:29:18 +0530

    added logic for showing prebooking train details on trip creation

--- src/components/tripCreation/trainDetails/data.js
+++ src/components/tripCreation/trainDetails/data.js
@@ -1,6 +1,16 @@
 const trainDetailsProps = {
   onPress: () => console.log('add pressed'),
-  data: undefined,
+  data: [
+    {
+      sourceStationCode: 'CSTM',
+      source: 'Mumbai',
+      destinationStationCode: 'TVC',
+      destination: 'Thiruvanthapuram',
+      departureDate: '2022-11-12',
+      startTime: '01:00 PM',
+      endTime: '04:00 PM',
+    },
+  ],
 };
 
 export default trainDetailsProps;
--- src/components/tripCreation/trainDetails/index.js
+++ src/components/tripCreation/trainDetails/index.js
@@ -2,15 +2,37 @@ import {View} from 'react-native';
 import React from 'react';
 import SubTripTitle from '../subTripTitle';
 import {Strings} from '../../../utils/strings/index.travelPlus';
+import Styles from './Styles';
+import TrainItineraryCard from '../../trips/itineraryCards/trainItineraryCard';
+import moment from 'moment';
 
 export default function TrainDetails({onPress, data, style}) {
+  const generateRequestInfo = (item) => ({
+    date: moment(item.departureDate).format('DD'),
+    month: moment(item.departureDate).format('MMM'),
+    slotDetail: `Slot: ${item.startTime} - ${item.endTime}`,
+    source: item.source,
+    destination: item.destination,
+    sourceStationCode: item.sourceStationCode,
+    destinationStationCode: item.destinationStationCode,
+  });
   return (
     <View style={style}>
       <SubTripTitle
         title={Strings.trains}
         onPress={onPress}
         dataLength={data?.length}
+        style={Styles.headerStyle(data)}
       />
+      {data?.map((item, index) => (
+        <TrainItineraryCard
+          tripRequest={generateRequestInfo(item)}
+          showPreBookingCard={true}
+          actionDisabled={true}
+          hideIcon
+          style={Styles.cardStyle(data, index)}
+        />
+      ))}
     </View>
   );
 }
