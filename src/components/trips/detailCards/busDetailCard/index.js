ff3bd849214b82ab8897513ac9dd8cf2d849e097
Nikhil Kumar Choudhary <nikhil.kumar@fabhotels.com>
2022-11-17 12:53:38 +0530

    added boolean check in bus detail card to fix crash

--- src/components/trips/detailCards/busDetailCard/index.js
+++ src/components/trips/detailCards/busDetailCard/index.js
@@ -244,8 +244,9 @@ const BusDetailCard = ({
                   ))}
                 </>
               )}
-            {(bookingDetails?.coordinatorNo ||
-              bookingDetails?.coordinatorName) && (
+            {!!(
+              bookingDetails?.coordinatorNo || bookingDetails?.coordinatorName
+            ) && (
               <>
                 <Separator style={Styles.separatorStyle} />
                 <View style={Styles.marginTop_12}>
