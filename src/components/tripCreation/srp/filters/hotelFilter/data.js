fb312e9cae1d85b031e0dd2c0fb54c5020af8954
Gurupad Mamadapur <gurupadmamadapur@gmail.com>
2022-12-30 13:14:01 +0530

    Hotel filter prop changes

--- src/components/tripCreation/srp/filters/hotelFilter/data.js
+++ src/components/tripCreation/srp/filters/hotelFilter/data.js
@@ -1,81 +1,70 @@
 const hotelFilterProps = {
   sortData: [
     {
-      id: 'NON_STOP',
-      value: 'Non-stop',
+      key: 'CHEAPEST',
+      value: 'Cheapest',
       selected: false,
     },
     {
-      id: 'EARLIEST_FIRST',
-      value: 'Earliest first',
+      key: 'POPULAR',
+      value: 'Popular',
       selected: true,
     },
     {
-      id: 'CHEAPEST_FIRST',
-      value: 'Cheapest first',
-      selected: false,
-    },
-    {
-      id: 'NON_STOP1',
-      value: 'Non-stop',
+      key: 'HIGHEST_RATING',
+      value: 'Highest rating',
       selected: false,
     },
   ],
   filterData: {
     entitlement: [
       {
-        id: 'IN_POLICY',
+        id: 'INPOLICY',
         value: 'In policy',
         selected: true,
       },
       {
-        id: 'OUT_OF_POLICY',
+        id: 'OUTOFPOLICY',
         value: 'Out of policy',
         selected: false,
       },
     ],
-    preferredType: [
+    travelPlusPreferredFilter: false,
+    contractedRatePreferredFilter: false,
+    starRatings: [
       {
-        id: 'CONTRACTED_RATE',
-        value: 'Contracted rate',
+        id: '1',
+        name: '1 Star',
         selected: false,
       },
-      {
-        id: 'TRAVELPLUS_PREFERRED',
-        value: 'TravelPlus preferred',
-        selected: false,
-      },
-    ],
-    starRating: [
       {
         id: '2',
-        starRating: '<2',
-        selected: true,
+        name: '2 Star',
+        selected: false,
       },
       {
         id: '3',
-        starRating: '3',
+        name: '3 Star',
         selected: false,
       },
       {
         id: '4',
-        starRating: '4',
-        selected: true,
+        name: '4 Star',
+        selected: false,
       },
       {
         id: '5',
-        starRating: '5',
+        name: '5 Star',
         selected: false,
       },
     ],
     priceData: {
-      minLimit: 2000,
-      maxLimit: 40000,
-      granularity: 400,
-      minValue: 6000,
-      maxValue: 13000,
-      onChange: (initialValue, finalValue) =>
-        console.log('slider moved', initialValue, finalValue),
+      minLimit: 0,
+      maxLimit: 1000000,
+      granularity: 100,
+      minValue: 0,
+      maxValue: 1000000,
+      changed: false,
     },
   },
 };
--- src/components/tripCreation/srp/filters/hotelFilter/index.js
+++ src/components/tripCreation/srp/filters/hotelFilter/index.js
@@ -9,59 +9,51 @@ import FText from '../../../../../common/rn/FText';
 import {Color} from '../../../../../utils/color/index.travelPlus';
 import RangeSlider from '../../../../../common/components/rangeSlider';
 
-const HotelFilter = ({
-  sortData,
-  onSortSelect,
-  // entitlementData,
-  onEntitlementSelect,
-  // preferredTypeData,
-  onPreferredTypeSelect,
-  // hotelRatingData,
-  // priceData,
-  onApply,
-  onClearAll,
-  filterData,
-  // onHotelRatingSelect,
-}) => {
+const HotelFilter = ({sortData, filterData, onSortSelect, onApply}) => {
   return (
     <SortAndFilter
       sortData={sortData}
       onSortSelect={onSortSelect}
-      onApply={onApply}
-      onClearAll={onClearAll}>
+      onApply={onApply}>
       <FilterSection title={Strings.price}>
         <RangeSlider {...filterData.priceData} />
       </FilterSection>
       <FilterSection title={Strings.hotelRating}>
         <View style={Styles.buttonContainer}>
-          {filterData.starRating.map((item, index) => (
-            <FilterButton
-              isSelected={item.selected}
-              // onPress={() => onHotelRatingSelect(item)}
-              viewStyle={Styles.flexRow}
-              addMarginRight={index < filterData.starRating.length - 1}>
-              <Icon.RatingStar
-                width={DP._16}
-                height={DP._16}
-                fill={item.selected ? Color.DODGER_BLUE : null}
-              />
-              <FText style={Styles.ratingText(item.selected)}>
-                {item.starRating}
-              </FText>
-            </FilterButton>
-          ))}
+          {filterData.starRatings
+            .filter((e) => e.id !== '1')
+            .map((item, index) => (
+              <FilterButton
+                isSelected={item.selected}
+                // onPress={() => onHotelRatingSelect(item)}
+                viewStyle={Styles.flexRow}
+                addMarginRight={index < filterData.starRatings.length - 1}>
+                <Icon.RatingStar
+                  width={DP._16}
+                  height={DP._16}
+                  fill={item.selected ? Color.DODGER_BLUE : null}
+                />
+                <FText style={Styles.ratingText(item.selected)}>
+                  {item.id === '2' ? '<2' : item.id}
+                </FText>
+              </FilterButton>
+            ))}
         </View>
       </FilterSection>
       <FilterSection title={Strings.preferredType}>
         <View style={Styles.buttonContainer}>
-          {filterData.preferredType.map((item, index) => (
-            <FilterButton
-              isSelected={item.selected}
-              onPress={() => onPreferredTypeSelect(item)}
-              addMarginRight={index < filterData.preferredType.length - 1}>
-              {item.value}
-            </FilterButton>
-          ))}
+          <FilterButton
+            isSelected={filterData.selected}
+            onPress={() => {}}
+            addMarginRight={true}>
+            {'Contracted rate'}
+          </FilterButton>
+          <FilterButton
+            isSelected={filterData.travelPlusPreferredFilter}
+            onPress={() => {}}
+            addMarginRight={false}>
+            {'TravelPlus preferred'}
+          </FilterButton>
         </View>
       </FilterSection>
       <FilterSection
@@ -71,7 +63,7 @@ const HotelFilter = ({
           {filterData.entitlement.map((item, index) => (
             <FilterButton
               isSelected={item.selected}
-              onPress={() => onEntitlementSelect(item)}
+              onPress={() => {}}
               addMarginRight={index < filterData.entitlement.length - 1}>
               {item.value}
             </FilterButton>
