c2eadb970852153c58f5561dc1f7ebc8c44d8d28
Gurupad Mamadapur <gurupadmamadapur@gmail.com>
2023-01-09 15:31:09 +0530

    Make useTimer hook common and increase interval. Also preset the deadline

--- src/utils/strings/common.js
+++ src/utils/strings/common.js
@@ -170,8 +170,11 @@ export default {
     paidAmount: 'Paid amount',
   },
   ok: 'Ok',
-  makePaymentAfterTimeOut:
+  leftToPayElseApprovalCancel: 'left to pay. Else approval shall be cancelled',
+  makePaymentElseTripCancel:
     'Hurry up! Make the payment now else your trip will be cancelled.',
+  makePaymentElseApprovalCancel:
+    'Hurry up! Make the payment now else your approval shall be cancelled.',
   makePaymentWithin: 'Make the payment within ',
   elsePaymentCancel: 'else your trip will be cancelled.',
 };
