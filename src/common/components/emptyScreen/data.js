7c33321ef277869482e5194a74b96a070fc21b60
Gurupad Mamadapur <gurupadmamadapur@gmail.com>
2023-01-12 20:21:03 +0530

    Use NO_COLOR enum and pass btnType prop to EmptyScreen

--- src/common/components/emptyScreen/data.js
+++ src/common/components/emptyScreen/data.js
@@ -1,8 +1,11 @@
+import {BUTTON_TYPE} from '../button';
+
 const emptyScreenProps = {
   heading: 'This list is empty',
   image: null,
   subHeading: 'This list is empty',
   btnText: 'Refresh',
+  btnType: BUTTON_TYPE.PRIMARY,
   onPress: () => console.log('Button Pressed'),
 };
 
