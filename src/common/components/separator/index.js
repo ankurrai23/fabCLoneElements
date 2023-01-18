ed8f41869899fce45f7c53eac44cba17a3aed0a7
Nikhil Kumar Choudhary <nikhil.kumar@fabhotels.com>
2023-01-18 11:30:13 +0530

    added container style in separator

--- src/common/components/separator/index.js
+++ src/common/components/separator/index.js
@@ -3,8 +3,20 @@ import {View} from 'react-native';
 
 import Styles from './Styles';
 
-function Separator({style, ...props}) {
-  return <View style={[Styles.container, style]} {...props} />;
+/*
+'containerStyle' was added to handle cases where we want to give a background color to separator.
+Has been used in flight itinerary card
+*/
+
+function Separator({containerStyle, style, ...props}) {
+  if (containerStyle) {
+    return (
+      <View style={containerStyle} {...props}>
+        <View style={[Styles.container, style]} />
+      </View>
+    );
+  }
+  return <View style={[Styles.container, style]} />;
 }
 
 export default Separator;
