8b43bcf5f5ea3b53d58afb65e3428d834e352082
Nikhil Kumar Choudhary <nikhil.kumar@fabhotels.com>
2023-02-27 18:21:42 +0530

    updated pending payment sheet

--- src/components/home/pendingPaymentSheet/index.js
+++ src/components/home/pendingPaymentSheet/index.js
@@ -1,5 +1,11 @@
-import {View, FlatList, LayoutAnimation} from 'react-native';
-import React, {useState, useImperativeHandle, useRef, useEffect} from 'react';
+import {View, LayoutAnimation} from 'react-native';
+import React, {
+  useState,
+  useImperativeHandle,
+  useRef,
+  useEffect,
+  useCallback,
+} from 'react';
 import {DP} from '../../../utils/Dimen';
 import FText, {FONT_TYPE} from '../../../common/rn/FText';
 import FTouchableOpacity from '../../../common/rn/FTouchableOpacity';
@@ -7,8 +13,7 @@ import Button from '../../../common/components/button';
 import Styles, {CARD_WIDTH} from './Styles';
 import {Strings} from '../../../utils/strings/index.travelPlus';
 import {Color} from '../../../utils/color/index.travelPlus';
-import FBottomSheet from '../../../common/rn/FBottomSheet';
-
+import {FlatList} from 'react-native-gesture-handler';
 const defaultTripTitle = {
   timeLeft: '',
   alertString: '',
@@ -19,8 +24,8 @@ export const Timer = React.forwardRef(({paymentRequests}, ref) => {
 
   useEffect(() => {
     const newAlertMessage = {
-      timeLeft: paymentRequests[currentIndex].deadLine?.key,
-      alertString: paymentRequests[currentIndex].deadLine?.value,
+      timeLeft: paymentRequests?.[currentIndex]?.deadLine?.key,
+      alertString: paymentRequests?.[currentIndex]?.deadLine?.value,
     };
     setAlertMessage(newAlertMessage);
   }, [currentIndex, paymentRequests]);
@@ -64,92 +69,86 @@ export const CardDots = React.forwardRef(
 
 const ItemSeparator = () => <View style={{width: DP._16}} />;
 
-const PendingPaymentSheet = React.forwardRef(
-  ({paymentRequests, onPressPayment}, ref) => {
-    const cardDotsRef = useRef();
-    const timerRef = useRef();
+const PendingPaymentSheet = ({paymentRequests, onPressPayment}) => {
+  const cardDotsRef = useRef();
+  const timerRef = useRef();
 
-    const viewabilityConfig = {
-      itemVisiblePercentThreshold: 50,
-      waitForInteraction: true,
-    };
+  const viewabilityConfig = {
+    itemVisiblePercentThreshold: 50,
+    waitForInteraction: true,
+  };
 
-    const onViewableItemsChanged = (info) => {
-      if (info.viewableItems.length > 0) {
-        timerRef.current.setIndex(info.viewableItems[0].index);
-        cardDotsRef.current.setIndex(info.viewableItems[0].index);
-      }
-    };
+  const onViewableItemsChanged = useCallback((info) => {
+    if (info.viewableItems.length > 0) {
+      timerRef.current.setIndex(info.viewableItems[0].index);
+      cardDotsRef.current.setIndex(info.viewableItems[0].index);
+    }
+  }, []);
 
-    const renderItem = ({item}) => (
-      <FTouchableOpacity
-        style={
-          paymentRequests.length === 1
-            ? Styles.cardStyleSingle
-            : Styles.cardStyle
-        }>
-        <View style={Styles.flexRow}>
-          <View>
-            <FText>
-              {Strings.tripId}
-              <FText type={FONT_TYPE.MEDIUM}>{item.masterTripId}</FText>
-            </FText>
-            <FText style={Styles.tripTitleStyle}>{item.tripTitle}</FText>
-            <FText>
-              {item.start} - {item.end}
-            </FText>
-          </View>
-          <View style={Styles.priceContainer}>
-            <FText type={FONT_TYPE.MEDIUM} style={Styles.priceStyle}>
-              {Strings.rupee}
-              {Math.ceil(item.amount)}
-            </FText>
-            <FText style={Styles.inclGSTStyle}>{Strings.inclGST}</FText>
-          </View>
-        </View>
-        <Button
-          textFont={FONT_TYPE.MEDIUM}
-          style={Styles.buttonStyle}
-          onPress={() => {
-            onPressPayment(item.masterTripId);
-          }}>
-          {Strings.pay}
-        </Button>
-      </FTouchableOpacity>
-    );
-
-    return (
-      <FBottomSheet ref={ref}>
-        <View style={Styles.container}>
-          <FText type={FONT_TYPE.MEDIUM} style={Styles.title}>
-            {Strings.paymentPending}
+  const renderItem = ({item}) => (
+    <FTouchableOpacity
+      style={
+        paymentRequests.length === 1 ? Styles.cardStyleSingle : Styles.cardStyle
+      }>
+      <View style={Styles.flexRow}>
+        <View>
+          <FText>
+            {Strings.tripId}
+            <FText type={FONT_TYPE.MEDIUM}>{item.masterTripId}</FText>
           </FText>
-          <Timer ref={timerRef} paymentRequests={paymentRequests} />
-          <FlatList
-            data={paymentRequests}
-            decelerationRate={'fast'}
-            horizontal={true}
-            bounces={false}
-            pagingEnabled={true}
-            disableIntervalMomentum={true}
-            snapToInterval={CARD_WIDTH + DP._16}
-            showsHorizontalScrollIndicator={false}
-            viewabilityConfig={viewabilityConfig}
-            onViewableItemsChanged={onViewableItemsChanged}
-            contentContainerStyle={Styles.flatlistContainerStyle}
-            renderItem={renderItem}
-            ItemSeparatorComponent={ItemSeparator}
-          />
-          {paymentRequests.length !== 1 && (
-            <CardDots
-              ref={cardDotsRef}
-              data={paymentRequests}
-              dotColor={Color.TWILIGHT_BLUE}
-            />
-          )}
+          <FText style={Styles.tripTitleStyle}>{item.tripTitle}</FText>
+          <FText>
+            {item.start} - {item.end}
+          </FText>
         </View>
-      </FBottomSheet>
-    );
-  },
-);
+        <View style={Styles.priceContainer}>
+          <FText type={FONT_TYPE.MEDIUM} style={Styles.priceStyle}>
+            {Strings.rupee}
+            {Math.ceil(item.amount)}
+          </FText>
+          <FText style={Styles.inclGSTStyle}>{Strings.inclGST}</FText>
+        </View>
+      </View>
+      <Button
+        textFont={FONT_TYPE.MEDIUM}
+        style={Styles.buttonStyle}
+        onPress={() => {
+          onPressPayment(item.masterTripId);
+        }}>
+        {Strings.pay}
+      </Button>
+    </FTouchableOpacity>
+  );
+
+  return (
+    <View style={Styles.container}>
+      <FText type={FONT_TYPE.MEDIUM} style={Styles.title}>
+        {Strings.paymentPending}
+      </FText>
+      <Timer ref={timerRef} paymentRequests={paymentRequests} />
+      <FlatList
+        data={paymentRequests}
+        decelerationRate={'fast'}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+        disableIntervalMomentum={true}
+        snapToInterval={CARD_WIDTH + DP._16}
+        showsHorizontalScrollIndicator={false}
+        viewabilityConfig={viewabilityConfig}
+        onViewableItemsChanged={onViewableItemsChanged}
+        contentContainerStyle={Styles.flatlistContainerStyle}
+        renderItem={renderItem}
+        ItemSeparatorComponent={ItemSeparator}
+      />
+      {paymentRequests.length !== 1 && (
+        <CardDots
+          ref={cardDotsRef}
+          data={paymentRequests}
+          dotColor={Color.TWILIGHT_BLUE}
+        />
+      )}
+    </View>
+  );
+};
 export default PendingPaymentSheet;
