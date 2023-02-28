fb4e6f18fd973ac061347fe3aaa6c808e3730c22
Nikhil Kumar Choudhary <nikhil.kumar@fabhotels.com>
2023-02-28 12:12:47 +0530

    removed bottom sheet from selectRoomSheet, selector and priceBreakupSheet

--- src/common/components/selector/index.js
+++ src/common/components/selector/index.js
@@ -6,48 +6,38 @@ import FTouchableOpacity from '../../rn/FTouchableOpacity';
 import Separator from '../separator';
 import Styles from './Styles';
 import {dialogBoxStyle} from '../../../utils/Utils';
-import FBottomSheet from '../../rn/FBottomSheet';
 
-const Selector = React.forwardRef(
-  (
-    {
-      title,
-      data,
-      multiSelect,
-      keySelected,
-      onItemSelect,
-      containerStyle,
-      itemStyle,
-    },
-    ref,
-  ) => {
-    return (
-      <FBottomSheet ref={ref}>
-        <View style={[Styles.container, containerStyle]}>
-          <FText style={Styles.titleText}>{title}</FText>
-          <FlatList
-            data={data}
-            renderItem={({item}) => {
-              const isSelected = keySelected === item.key;
-              return (
-                <FTouchableOpacity
-                  onPress={() => onItemSelect(item)}
-                  style={[Styles.itemStyle, itemStyle]}>
-                  <FText style={Styles.valueText}>{item.value}</FText>
-                  {isSelected ? <Icon.RadioActive /> : <Icon.RadioPassive />}
-                </FTouchableOpacity>
-              );
-            }}
-            ItemSeparatorComponent={() => (
-              <Separator style={Styles.separator} />
-            )}
-            style={{...dialogBoxStyle()}}
-            bounces={false}
-          />
-        </View>
-      </FBottomSheet>
-    );
-  },
-);
+const Selector = ({
+  title,
+  data,
+  multiSelect,
+  keySelected,
+  onItemSelect,
+  containerStyle,
+  itemStyle,
+}) => {
+  return (
+    <View style={[Styles.container, containerStyle]}>
+      <FText style={Styles.titleText}>{title}</FText>
+      <FlatList
+        data={data}
+        renderItem={({item}) => {
+          const isSelected = keySelected === item.key;
+          return (
+            <FTouchableOpacity
+              onPress={() => onItemSelect(item)}
+              style={[Styles.itemStyle, itemStyle]}>
+              <FText style={Styles.valueText}>{item.value}</FText>
+              {isSelected ? <Icon.RadioActive /> : <Icon.RadioPassive />}
+            </FTouchableOpacity>
+          );
+        }}
+        ItemSeparatorComponent={() => <Separator style={Styles.separator} />}
+        style={{...dialogBoxStyle()}}
+        bounces={false}
+      />
+    </View>
+  );
+};
 
 export default Selector;
--- src/components/trips/priceBreakupSheet/index.js
+++ src/components/trips/priceBreakupSheet/index.js
@@ -3,142 +3,140 @@ import {View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import Button from '../../../common/components/button';
 import Separator from '../../../common/components/separator';
-import FBottomSheet from '../../../common/rn/FBottomSheet';
 import FText, {FONT_TYPE} from '../../../common/rn/FText';
 import FTouchableOpacity from '../../../common/rn/FTouchableOpacity';
 import {Strings} from '../../../utils/strings/index.travelPlus';
 import {formattedPrice} from '../../../utils/Utils';
 import Styles from './Styles';
 
-const PriceBreakupSheet = React.forwardRef(
-  ({data, containerStyle, onOkPress}, ref) => {
-    const FeeDetail = ({title, subtitle, cost, type, style}) => (
-      <FTouchableOpacity style={[Styles.feeContainer, style]}>
-        <View style={Styles.flexRow}>
-          <FText type={type}>{title}</FText>
-          {subtitle && <FText style={Styles.subTitle}>{subtitle}</FText>}
-        </View>
-        <FText type={type}>{formattedPrice(cost)}</FText>
-      </FTouchableOpacity>
-    );
+const PriceBreakupSheet = ({data, containerStyle, onOkPress}) => {
+  if (!data) {
+    return null;
+  }
+  const FeeDetail = ({title, subtitle, cost, type, style}) => (
+    <FTouchableOpacity style={[Styles.feeContainer, style]}>
+      <View style={Styles.flexRow}>
+        <FText type={type}>{title}</FText>
+        {subtitle && <FText style={Styles.subTitle}>{subtitle}</FText>}
+      </View>
+      <FText type={type}>{formattedPrice(cost)}</FText>
+    </FTouchableOpacity>
+  );
 
-    return (
-      <FBottomSheet ref={ref}>
-        <View style={[Styles.container, containerStyle]}>
-          <FText style={Styles.titleText}>{Strings.priceBreakup.title}</FText>
-          <ScrollView>
-            {!!data.flightBreakup && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.flightCharges}
-                  cost={data.flightBreakup.flightCharges}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.convenienceFee}
-                  cost={data.flightBreakup.convFeeWithGst}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.hotelBreakup && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.hotelCharges}
-                  cost={data.hotelBreakup.hotelCharges}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.gst}
-                  cost={data.hotelBreakup.gst}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.convenienceFee}
-                  cost={data.hotelBreakup.convFee}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.hotelCancellationCharges?.amount && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.hotelCancellationCharges}
-                  cost={data.hotelCancellationCharges.amount}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.convenienceFee}
-                  cost={data.hotelCancellationCharges.convFeeWithGst}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.hotelModificationCharges?.amount && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.hotelModificationCharges}
-                  subtitle={Strings.priceBreakup.included}
-                  cost={data.hotelModificationCharges.amount}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.convenienceFee}
-                  cost={data.hotelModificationCharges.convFeeWithGst}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.flightCancellationCharges?.amount && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.flightCancellationCharges}
-                  cost={data.flightCancellationCharges.amount}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.convenienceFee}
-                  cost={data.flightCancellationCharges.convFeeWithGst}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.flightModificationCharges?.amount && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.flightModificationCharges}
-                  cost={data.flightModificationCharges.amount}
-                />
-                <FeeDetail
-                  title={Strings.priceBreakup.convenienceFee}
-                  cost={data.flightModificationCharges.convFeeWithGst}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.grandTotal && (
-              <>
-                <FeeDetail
-                  title={Strings.priceBreakup.totalAmount}
-                  cost={data.grandTotal}
-                  type={FONT_TYPE.MEDIUM}
-                />
-                <Separator style={Styles.separator} />
-              </>
-            )}
-            {!!data.paidAmount && (
-              <FeeDetail
-                title={Strings.priceBreakup.paidAmount}
-                cost={data.paidAmount}
-                style={Styles.paidAmountText}
-              />
-            )}
-          </ScrollView>
-          <Button
-            textFont={FONT_TYPE.MEDIUM}
-            textStyle={Styles.buttonText}
-            onPress={onOkPress}
-            style={Styles.buttonStyle}>
-            {Strings.ok}
-          </Button>
-        </View>
-      </FBottomSheet>
-    );
-  },
-);
+  return (
+    <View style={[Styles.container, containerStyle]}>
+      <FText style={Styles.titleText}>{Strings.priceBreakup.title}</FText>
+      <ScrollView>
+        {!!data.flightBreakup && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.flightCharges}
+              cost={data.flightBreakup.flightCharges}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.convenienceFee}
+              cost={data.flightBreakup.convFeeWithGst}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.hotelBreakup && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.hotelCharges}
+              cost={data.hotelBreakup.hotelCharges}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.gst}
+              cost={data.hotelBreakup.gst}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.convenienceFee}
+              cost={data.hotelBreakup.convFee}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.hotelCancellationCharges?.amount && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.hotelCancellationCharges}
+              cost={data.hotelCancellationCharges.amount}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.convenienceFee}
+              cost={data.hotelCancellationCharges.convFeeWithGst}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.hotelModificationCharges?.amount && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.hotelModificationCharges}
+              subtitle={Strings.priceBreakup.included}
+              cost={data.hotelModificationCharges.amount}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.convenienceFee}
+              cost={data.hotelModificationCharges.convFeeWithGst}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.flightCancellationCharges?.amount && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.flightCancellationCharges}
+              cost={data.flightCancellationCharges.amount}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.convenienceFee}
+              cost={data.flightCancellationCharges.convFeeWithGst}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.flightModificationCharges?.amount && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.flightModificationCharges}
+              cost={data.flightModificationCharges.amount}
+            />
+            <FeeDetail
+              title={Strings.priceBreakup.convenienceFee}
+              cost={data.flightModificationCharges.convFeeWithGst}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.grandTotal && (
+          <>
+            <FeeDetail
+              title={Strings.priceBreakup.totalAmount}
+              cost={data.grandTotal}
+              type={FONT_TYPE.MEDIUM}
+            />
+            <Separator style={Styles.separator} />
+          </>
+        )}
+        {!!data.paidAmount && (
+          <FeeDetail
+            title={Strings.priceBreakup.paidAmount}
+            cost={data.paidAmount}
+            style={Styles.paidAmountText}
+          />
+        )}
+      </ScrollView>
+      <Button
+        textFont={FONT_TYPE.MEDIUM}
+        textStyle={Styles.buttonText}
+        onPress={onOkPress}
+        style={Styles.buttonStyle}>
+        {Strings.ok}
+      </Button>
+    </View>
+  );
+};
 
 export default PriceBreakupSheet;
