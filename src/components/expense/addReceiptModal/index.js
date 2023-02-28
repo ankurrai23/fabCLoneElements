adb373244749b7cfa15c51c88d629f65ec630481
Nikhil Kumar Choudhary <nikhil.kumar@fabhotels.com>
2023-02-28 17:28:27 +0530

    removed bottomsheet from addReceipt modal

--- src/components/expense/addReceiptModal/index.js
+++ src/components/expense/addReceiptModal/index.js
@@ -9,34 +9,31 @@ import Styles from './Styles';
 import {PICKER_TYPE} from '../../../utils/Constants';
 import {Strings} from '../../../utils/strings/index.travelPlus';
 import Icon from '../../../assets/icons/Icon';
-import FBottomSheet from '../../../common/rn/FBottomSheet';
 
-const AddReceiptModal = React.forwardRef((props, ref) => {
+const AddReceiptModal = (props) => {
   return (
-    <FBottomSheet ref={ref}>
-      <View style={Styles.container}>
-        <FText style={Styles.head}>{Strings.addAReceipt}</FText>
-        <FTouchableOpacity
-          style={Styles.line}
-          onPress={() => props.onPress(PICKER_TYPE.CAMERA)}>
-          <Icon.Camera style={Styles.icon} />
-          <FText style={Styles.option}>{Strings.openCamera}</FText>
-        </FTouchableOpacity>
-        <FTouchableOpacity
-          style={Styles.line}
-          onPress={() => props.onPress(PICKER_TYPE.GALLERY)}>
-          <Icon.Image style={Styles.icon} />
-          <FText style={Styles.option}>{Strings.uploadFromGallery}</FText>
-        </FTouchableOpacity>
-        <FTouchableOpacity
-          style={Styles.line}
-          onPress={() => props.onPress(PICKER_TYPE.FILE_MANAGER)}>
-          <Icon.Upload style={Styles.icon} />
-          <FText style={Styles.option}>{Strings.importFromFiles}</FText>
-        </FTouchableOpacity>
-      </View>
-    </FBottomSheet>
+    <View style={Styles.container}>
+      <FText style={Styles.head}>{Strings.addAReceipt}</FText>
+      <FTouchableOpacity
+        style={Styles.line}
+        onPress={() => props.onPress(PICKER_TYPE.CAMERA)}>
+        <Icon.Camera style={Styles.icon} />
+        <FText style={Styles.option}>{Strings.openCamera}</FText>
+      </FTouchableOpacity>
+      <FTouchableOpacity
+        style={Styles.line}
+        onPress={() => props.onPress(PICKER_TYPE.GALLERY)}>
+        <Icon.Image style={Styles.icon} />
+        <FText style={Styles.option}>{Strings.uploadFromGallery}</FText>
+      </FTouchableOpacity>
+      <FTouchableOpacity
+        style={Styles.line}
+        onPress={() => props.onPress(PICKER_TYPE.FILE_MANAGER)}>
+        <Icon.Upload style={Styles.icon} />
+        <FText style={Styles.option}>{Strings.importFromFiles}</FText>
+      </FTouchableOpacity>
+    </View>
   );
-});
+};
 
 export default AddReceiptModal;
