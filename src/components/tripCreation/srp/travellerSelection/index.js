ad5be070fe8a1788cd4dfadc7106555b7c513114
Chandan Agarwal <chandanagarwal@Chandans-MacBook-Pro.local>
2023-04-02 14:00:05 +0530

    added autoScrolling to index for TravellerSelection Component

--- src/components/tripCreation/srp/travellerSelection/index.js
+++ src/components/tripCreation/srp/travellerSelection/index.js
@@ -1,16 +1,22 @@
-import {View, Text, FlatList} from 'react-native';
-import React from 'react';
+import {View, FlatList} from 'react-native';
+import React, {useRef} from 'react';
 import Styles from './Styles';
 import {DP} from '../../../../utils/Dimen';
 import Icon from '../../../../assets/icons/Icon';
 import FText from '../../../../common/rn/FText';
 import FTouchableOpacity from '../../../../common/rn/FTouchableOpacity';
+
+let selectedIndex;
 const TravellerSelection = ({
   selectionPlaceHolder,
   travellersInfo,
   onClick,
 }) => {
+  const flatListRef = useRef();
   const renderTraveller = ({item, index}) => {
+    if (item.isSelected) {
+      selectedIndex = index;
+    }
     return (
       <FTouchableOpacity
         style={Styles.travellerContainer(index === 0, item.isSelected)}
@@ -31,18 +37,18 @@ const TravellerSelection = ({
 
         <FTouchableOpacity
           style={Styles.trashCanContainer(item.selectedData, item.isSelected)}
-          onPress={
-            item.selectedData
-              ? () => {
-                  onClick(index, true);
-                }
-              : null
-          }>
+          onPress={() => onClick(index, true)}>
           {!!item.selectedData && <Icon.Trash width={DP._14} height={DP._14} />}
         </FTouchableOpacity>
       </FTouchableOpacity>
     );
   };
+  setTimeout(() =>
+    flatListRef?.current.scrollToIndex({
+      animated: true,
+      index: selectedIndex,
+    }),
+  );
   return (
     <View style={Styles.container}>
       <FlatList
@@ -51,6 +57,17 @@ const TravellerSelection = ({
         horizontal={true}
         showsHorizontalScrollIndicator={false}
         bounces={false}
+        ref={flatListRef}
+        onScrollToIndexFailed={(info) => {
+          setTimeout(
+            () =>
+              flatListRef?.current.scrollToIndex({
+                animated: true,
+                index: selectedIndex,
+              }),
+            100,
+          );
+        }}
       />
     </View>
   );
